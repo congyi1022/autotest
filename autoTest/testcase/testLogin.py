a97a48978817d1d21372b3c1e11576fca100d4ae
licongyi <licongyi@wecash.net>
2020-08-16 18:18:36 +0800

    a

--- autoTest/testcase/testLogin.py
+++ autoTest/testcase/testLogin.py
@@ -3,12 +3,24 @@ from utils import sendRequest
 import unittest
 
 class TestLogin(unittest.TestCase):
+
+
     def setUp(self):
         self.sl= sendRequest.sendRequest()
 
-    def testErrorEmailandErrorPW(self):
+    def testRightEmailandRightPW(self):
         res=self.sl.sendrequest("licongyi@wecash.net","123456")
         self.assertEqual(200,res.status_code)
         print(res.text)
         self.assertIn("Logged in successfully",res.text)
+
+    def testRightEmailandErrorPW(self):
+        res = self.sl.sendrequest("licongyi@wecash.net", "111111")
+        self.assertEqual(200, res.status_code)
+        print(res.text)
+        self.assertIn("Wrong credentials", res.text)
+
+if __name__=="__main__":
+    unittest.main()
+
 
