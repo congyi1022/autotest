e6614a056b2e3336cecc22dbc3914c08dd3a9cec
licongyi <licongyi@wecash.net>
2020-08-20 18:09:23 +0800

    封装请求成功

--- autoTest/testcase/testLoginWithExcel.py
+++ autoTest/testcase/testLoginWithExcel.py
@@ -1,19 +1,33 @@
 # coding:utf-8
 from utils import SendRequest
 import unittest
-import xlrd
 from utils.readExcel import ReadExcel
+from utils import getTokenAndCookie
+import json
+
 
 class testLoginwithExcel(unittest.TestCase):
-    dataLists=ReadExcel().get_excel()
-    print(dataLists)
+    dataLists = ReadExcel().get_excel()
 
-    def setUp(self) :
-        self.sl = SendRequest.SendRequest()
+    def setUp(self):
+        self.sl = SendRequest.RunMain()
 
     def testRunTests(self):
         for testdata in self.dataLists:
-            with self.subTest(msg=testdata[0]):
-                res = self.sl.sendRequest(testdata[1], testdata[2])
-                self.assertEqual(testdata[3], int(res.status_code))
-                self.assertIn(testdata[4], res.text)
+            name = testdata[0]
+            url = testdata[1]
+            postdata = testdata[2]
+            status_code = testdata[3]
+            status_message = testdata[4]
+            method = testdata[5]
+
+            datas = getTokenAndCookie.getToken(url)
+            crsf_token = datas["token"]
+            post_data = json.loads(postdata)
+            post_data["csrf_token"] = crsf_token
+            cookies = datas["cookie"]
+
+            with self.subTest(msg=name):
+                res = self.sl.main_request(method, url, post_data, cookies)
+                self.assertEqual(status_code, int(res.status_code))
+                self.assertIn(status_message, res.text)
--- autoTest/utils/SendRequest.py
+++ autoTest/utils/SendRequest.py
@@ -1,36 +1,21 @@
 # coding:utf-8
 import requests
-from bs4 import BeautifulSoup
-import logging
-from utils import getTokenAndCookie
-from utils import readExcel
-import json
-from requests.cookies import RequestsCookieJar
 
 
 class RunMain:
-
-    def sendRequest(self, url, postData):
-
-        datas = getTokenAndCookie.getToken(url)
-        crsf_token = datas["token"]
-        post_data = json.loads(postData)
-        post_data["csrf_token"] = crsf_token
-        res = requests.post(url, data=post_data, cookies=datas["cookie"])
-        return res
 
     def sendGetRequest(self, url, data=None):
         res = requests.get(url, data)
         return res
 
-    def sendPostRequest(self, url, data, cookie=None):
-        res = requests.post(url, data, cookie)
+    def sendPostRequest(self, url, post_data, cookie):
+        res = requests.post(url, data=post_data, cookies=cookie)
         return res
 
     def main_request(self, method, url, data=None, cookie=None):
         res = None
         if method == "GET":
-            self.sendGetRequest(url, data)
+            res = self.sendGetRequest(url, data)
         elif method == "POST":
-            self.sendPostRequest(url, data, cookie)
+            res = self.sendPostRequest(url, data, cookie)
         return res
