f36ef864a54ef11671ae8029a4d19df8ff099c9a
licongyi <licongyi@wecash.net>
2020-08-20 14:13:14 +0800

    封装post参数

--- autoTest/utils/SendRequest.py
+++ autoTest/utils/SendRequest.py
@@ -8,12 +8,29 @@ import json
 from requests.cookies import RequestsCookieJar
 
 
-class SendRequest:
+class RunMain:
 
-       def sendRequest(self, url, postData):
+    def sendRequest(self, url, postData):
+
         datas = getTokenAndCookie.getToken(url)
         crsf_token = datas["token"]
         post_data = json.loads(postData)
         post_data["csrf_token"] = crsf_token
         res = requests.post(url, data=post_data, cookies=datas["cookie"])
+        return res
+
+    def sendGetRequest(self, url, data=None):
+        res = requests.get(url, data)
+        return res
+
+    def sendPostRequest(self, url, data, cookie=None):
+        res = requests.post(url, data, cookie)
+        return res
+
+    def main_request(self, method, url, data=None, cookie=None):
+        res = None
+        if method == "GET":
+            self.sendGetRequest(url, data)
+        elif method == "POST":
+            self.sendPostRequest(url, data, cookie)
         return res
